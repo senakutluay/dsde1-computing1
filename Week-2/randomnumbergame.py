c630526151c894051ca68e3dfb1ac7f524bc0efb
senakutluay <56586637+senakutluay@users.noreply.github.com>
2019-10-24 14:08:13 +0100

    random number game

---
+++ Week-2/randomnumbergame.py
@@ -0,0 +1,25 @@
+##this is just to see this little input thing
+#question=input('what is your message?  ')
+#print(question)
+
+import random
+
+print('what')
+secretNumber=random.randint(1, 10)
+
+for guessestaken in range(1,6):
+    guess=int(input())
+
+    if guess>secretNumber:
+        print('too hiigghh')
+    elif guess<secretNumber:
+        print('too low')
+    else:
+        break
+if secretNumber == guess:
+    print('congrats')
+else:
+    print('nope it was' + secretNumber+ 'Try again')
+
+
+
