de1135cf72487d51bb08a8aa9e4267b560a6685c
senakutluay <56586637+senakutluay@users.noreply.github.com>
2019-10-29 10:43:42 +0000

    lastque

--- Week-3/toys.py
+++ Week-3/toys.py
@@ -52,5 +52,5 @@ def is_even(a):
 def string_repeat(phrase, repeat):
     # hint: you can add strings together 
     # in order to concatenate them
-    return
+    return str(phrase * lastqrepeat)
 
