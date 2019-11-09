78f409e8278b14f92e446031e5c8067c936e8c55
senakutluay <56586637+senakutluay@users.noreply.github.com>
2019-11-09 12:27:14 +0000

    h

--- Week-3/fizzbuzz.py
+++ Week-3/fizzbuzz.py
@@ -2,16 +2,16 @@
 
 
 for a in range(0,101):
-    a=int(a)
+
     if (a % 3) == 0 and (a % 5) == 0:
         a='fizzbuzz'
         print(a)
     
-    if (a % 3) == 0 and (a % 5) != 0:
+    elif (a % 3) == 0 and (a % 5) != 0:
        a='fizz'
        print(a)
       
-    if (a % 5) == 0 and (a % 3) !=0:
+    elif (a % 5) == 0 and (a % 3) !=0:
         a='buzz'
         print(a)
     else:
