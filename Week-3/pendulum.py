c97e2ca8b44f4b34852c2c3ad3284eba97e67fcf
senakutluay <56586637+senakutluay@users.noreply.github.com>
2019-11-09 14:48:41 +0000

    h

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -3,16 +3,15 @@ import math
 def errors(length, gravity):
 
 
-    a= isinstance(length, int)
-    b= isinstance(gravity, int)
+    a= isinstance(length, (int, float,complex))
+    b= isinstance(gravity, (int, float, complex))
+    
+    
     if a == False:
         return TypeError
     elif b== False:
         return TypeError
-    elif a == True and b== True and a<0:
-        return ValueError
-    elif a == True and b== True and b<=0:
-        return ValueError
+
 def formula(x,y):
     return 2 * math.pi * math.sqrt(x/y)
 def period(length, gravity):
