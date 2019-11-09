09c50603a5c03d43159c7cdad7c98469bc0b9b0c
senakutluay <56586637+senakutluay@users.noreply.github.com>
2019-11-09 17:08:55 +0000

    f

--- Week-4/structures.py
+++ Week-4/structures.py
@@ -32,7 +32,7 @@ def part_reverse(the_list, beginning, end):
 # will return [0,1,2,3,3,3,4]. 
 def repeat_at_index(the_list, index):
     a=the_list[index]
-    for x in range index:
+    for x in range(index):
         multipliedlist=(x+1)*a
     firsthalf=the_list[0:index]
     lasthalf=the_list[index::]
